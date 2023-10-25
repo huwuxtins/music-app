6215519541e9d914bc9bf910c8636f572c8c372e
TranHuuLoc129 <loctran7129@gmail.com>
2023-10-25 19:16:57 +0700

    #25/10/2023 giao dien bat dau , chua co tinh nang

---
+++ app/src/main/java/com/example/musicapp/activities/Login.kt
@@ -0,0 +1,12 @@
+package com.example.musicapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.musicapp.R
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
